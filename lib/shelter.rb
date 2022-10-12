52d5f0d3cbed02e72721c324129446b436efc66f
beddings81 <111210465+beddings81@users.noreply.github.com>
2022-10-12 10:37:38 -0500

    Complete iteration1

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,9 +1,10 @@
 class Shelter
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :pets
 
   def initialize(name, capacity)
     @name = "Denver Animal Shelter"
     @capacity = capacity
+    @pets = []
   end
 
 
